912d001a59fdeeb93f8b9094a7b543ea98150665
Daniel del Castillo <155478862+DaniWallaceDev@users.noreply.github.com>
2024-05-01 08:42:08 +0000

    Modelo de DB Instagram acabado

--- src/models.py
+++ src/models.py
@@ -7,23 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    followers = Column(Integer)
+    following = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Usuario)
+
+class Comentarios(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    likes = Column(Integer)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+# Duda: podría relacionar mi tabla de likes ya generada con comentarios 
+# o tendría que crear una tabla para los likes de los comentarios?
 
     def to_dict(self):
         return {}
